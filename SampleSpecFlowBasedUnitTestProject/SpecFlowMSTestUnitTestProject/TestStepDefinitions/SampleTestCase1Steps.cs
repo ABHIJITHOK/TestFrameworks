25ed67d42e8c8293dbde843d818507588ce14410
Abhy.Kizhakkepat <Abhy.Kizhakkepat@tattsgroup.com>
2018-06-06 22:01:36 +1000

    Debugged tests

--- SampleSpecFlowBasedUnitTestProject/SpecFlowMSTestUnitTestProject/TestStepDefinitions/SampleTestCase1Steps.cs
+++ SampleSpecFlowBasedUnitTestProject/SpecFlowMSTestUnitTestProject/TestStepDefinitions/SampleTestCase1Steps.cs
@@ -7,22 +7,33 @@ namespace SpecFlowMSTestUnitTestProject.TestStepDefinitions
     [Binding]
     public partial class TestStepDefinitions
     {
+        private int value1 = 0, value2 = 0, sum = 0;
+
         [Given(@"I have entered (.*) into the calculator")]
         public void GivenIHaveEnteredIntoTheCalculator(int p0)
         {
             //TODO
+            if (value1 == 0)
+            {
+                value1 = p0;
+            }
+            else
+            {
+                value2 = p0;
+            }
         }
         
         [When(@"I press add")]
         public void WhenIPressAdd()
         {
             //TODO
+            sum = value1 + value2;
         }
 
         [Then(@"the result should be (.*) on the screen")]
         public void ThenTheResultShouldBeOnTheScreen(int p0)
         {
-            Assert.AreEqual(120, p0);
+            Assert.AreEqual(p0, sum);
         }
     }
 }
